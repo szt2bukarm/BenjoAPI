d0f28294c544266d0d53d60cfe35d4f4c9e18c12
Armin <admin@Armins-MacBook-Pro.local>
2025-02-27 19:45:50 +0100

    search fix

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -55,7 +55,7 @@ exports.search = async (req, res) => {
               name: item?.artists[0].name,
               id: item?.artists[0].id,
             },
-      image: item?.album.images[0].url,
+      image: item?.album?.images[0]?.url,
       trackName: item?.name,
       duration: item?.duration_ms,
     };
